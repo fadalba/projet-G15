1117374d4287f746ee1009c5a6e9966356fd2740
zoukana <assanediallo192@gmail.com>
2023-12-12 15:55:25 +0000

    aa

---
+++ frond_end/src/app/courbe/courbe.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CourbeComponent } from './courbe.component';
+
+describe('CourbeComponent', () => {
+  let component: CourbeComponent;
+  let fixture: ComponentFixture<CourbeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CourbeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CourbeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frond_end/src/app/courbe/courbe.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-courbe',
+  templateUrl: './courbe.component.html',
+  styleUrls: ['./courbe.component.css']
+})
+export class CourbeComponent {
+
+}
